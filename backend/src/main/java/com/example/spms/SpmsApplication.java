ccdb2b3a67e60e08e6bc91217d0b2906b912e43b
qbl-wqm <3053324297@qq.com>
2026-07-05 16:27:09 +0800

    fix: 添加 @MapperScan 注解扫描 Mapper 接口

--- backend/src/main/java/com/example/spms/SpmsApplication.java
+++ backend/src/main/java/com/example/spms/SpmsApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.spms.mapper")
 public class   SpmsApplication {
 
 	public static void main(String[] args) {
