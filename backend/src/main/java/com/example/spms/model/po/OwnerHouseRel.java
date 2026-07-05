c122fc3d8051e071e2fbc6bf2c95f2c1bf6b2032
tangjiuying <3195443214@qq.com>
2026-07-05 16:12:43 +0800

    feat: 完善Mapper接口

--- backend/src/main/java/com/example/spms/model/po/OwnerHouseRel.java
+++ backend/src/main/java/com/example/spms/model/po/OwnerHouseRel.java
@@ -1,12 +1,11 @@
 package com.example.spms.model.po;
 
-import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
 import java.io.Serializable;
 import java.util.Date;
-import lombok.Data;
 
 /**
  * @TableName owner_house_rel
@@ -15,96 +14,33 @@ import lombok.Data;
 @Data
 public class OwnerHouseRel implements Serializable {
     private Long id;
-
     private Long ownerInfoId;
-
     private Long houseInfoId;
-
     private Integer relationType;
-
     private Integer isPrimary;
-
     private Date moveInDate;
-
     private Integer isDeleted;
-
     private Integer version;
-
     private Date createTime;
-
     private Date updateTime;
-
     private Long createUser;
-
     private Long updateUser;
 
+    // ========== 扩展字段（非数据库字段，用于关联查询） ==========
+    @TableField(exist = false)
+    private String ownerName;           // 业主姓名
+
+    @TableField(exist = false)
+    private String ownerPhone;          // 业主电话
+
+    @TableField(exist = false)
+    private String houseNumber;         // 房号
+
+    @TableField(exist = false)
+    private String buildingName;        // 楼栋名称
+
+    @TableField(exist = false)
+    private String communityName;       // 小区名称
+
     private static final long serialVersionUID = 1L;
-
-    @Override
-    public boolean equals(Object that) {
-        if (this == that) {
-            return true;
-        }
-        if (that == null) {
-            return false;
-        }
-        if (getClass() != that.getClass()) {
-            return false;
-        }
-        OwnerHouseRel other = (OwnerHouseRel) that;
-        return (this.getId() == null ? other.getId() == null : this.getId().equals(other.getId()))
-            && (this.getOwnerInfoId() == null ? other.getOwnerInfoId() == null : this.getOwnerInfoId().equals(other.getOwnerInfoId()))
-            && (this.getHouseInfoId() == null ? other.getHouseInfoId() == null : this.getHouseInfoId().equals(other.getHouseInfoId()))
-            && (this.getRelationType() == null ? other.getRelationType() == null : this.getRelationType().equals(other.getRelationType()))
-            && (this.getIsPrimary() == null ? other.getIsPrimary() == null : this.getIsPrimary().equals(other.getIsPrimary()))
-            && (this.getMoveInDate() == null ? other.getMoveInDate() == null : this.getMoveInDate().equals(other.getMoveInDate()))
-            && (this.getIsDeleted() == null ? other.getIsDeleted() == null : this.getIsDeleted().equals(other.getIsDeleted()))
-            && (this.getVersion() == null ? other.getVersion() == null : this.getVersion().equals(other.getVersion()))
-            && (this.getCreateTime() == null ? other.getCreateTime() == null : this.getCreateTime().equals(other.getCreateTime()))
-            && (this.getUpdateTime() == null ? other.getUpdateTime() == null : this.getUpdateTime().equals(other.getUpdateTime()))
-            && (this.getCreateUser() == null ? other.getCreateUser() == null : this.getCreateUser().equals(other.getCreateUser()))
-            && (this.getUpdateUser() == null ? other.getUpdateUser() == null : this.getUpdateUser().equals(other.getUpdateUser()));
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
-        result = prime * result + ((getOwnerInfoId() == null) ? 0 : getOwnerInfoId().hashCode());
-        result = prime * result + ((getHouseInfoId() == null) ? 0 : getHouseInfoId().hashCode());
-        result = prime * result + ((getRelationType() == null) ? 0 : getRelationType().hashCode());
-        result = prime * result + ((getIsPrimary() == null) ? 0 : getIsPrimary().hashCode());
-        result = prime * result + ((getMoveInDate() == null) ? 0 : getMoveInDate().hashCode());
-        result = prime * result + ((getIsDeleted() == null) ? 0 : getIsDeleted().hashCode());
-        result = prime * result + ((getVersion() == null) ? 0 : getVersion().hashCode());
-        result = prime * result + ((getCreateTime() == null) ? 0 : getCreateTime().hashCode());
-        result = prime * result + ((getUpdateTime() == null) ? 0 : getUpdateTime().hashCode());
-        result = prime * result + ((getCreateUser() == null) ? 0 : getCreateUser().hashCode());
-        result = prime * result + ((getUpdateUser() == null) ? 0 : getUpdateUser().hashCode());
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", id=").append(id);
-        sb.append(", ownerInfoId=").append(ownerInfoId);
-        sb.append(", houseInfoId=").append(houseInfoId);
-        sb.append(", relationType=").append(relationType);
-        sb.append(", isPrimary=").append(isPrimary);
-        sb.append(", moveInDate=").append(moveInDate);
-        sb.append(", isDeleted=").append(isDeleted);
-        sb.append(", version=").append(version);
-        sb.append(", createTime=").append(createTime);
-        sb.append(", updateTime=").append(updateTime);
-        sb.append(", createUser=").append(createUser);
-        sb.append(", updateUser=").append(updateUser);
-        sb.append(", serialVersionUID=").append(serialVersionUID);
-        sb.append("]");
-        return sb.toString();
-    }
 }
